cfd163b2b70ffd56bc7228fd165c56c370b13261
Vikram Karve <vikram.karve@progress.com>
2023-01-24 09:48:45 +0530

    update pkg dep

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=meme-machine
 pkg_origin=vkarve_tryhabitat
-pkg_version="0.1.0"
+pkg_version="0.2.0"
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_deps=( core/imagemagick )
+pkg_deps=( vkarve_tryhabitat/imagemagick )
